f2378c80fe8084f28baf590ddd092e193b96d498
Abah Joseph <joefazee@gmail.com>
2023-01-04 13:03:20 +0100

    Ft/add more tests (#7)
    * add docker aws login
    
    * add tests for listAccount
    
    * add test for the transfer endpoint
    
    * add account tests and update deployment

--- api/account.go
+++ api/account.go
@@ -3,7 +3,6 @@ package api
 import (
 	"database/sql"
 	"errors"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -119,69 +118,4 @@ func (server *Server) listAccount(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, accounts)
 
-}
-
-func (server *Server) createTransfer(ctx *gin.Context) {
-
-	var req transferRequest
-	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return
-	}
-
-	fromAccount, valid := server.validAccount(ctx, req.FromAccountID, req.Currency)
-	if !valid {
-		return
-	}
-
-	if _, valid = server.validAccount(ctx, req.ToAccountID, req.Currency); !valid {
-		return
-	}
-
-	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
-	if fromAccount.Owner != authPayload.Username {
-		ctx.JSON(http.StatusBadRequest, errorResponse(errors.New("from account doesn`t belong to the authemticated user")))
-		return
-	}
-
-	if req.FromAccountID == req.ToAccountID {
-		ctx.JSON(http.StatusBadRequest, errorResponse(errors.New("origin and destination account is the same")))
-		return
-	}
-
-	arg := db.TransferTxParams{
-		FromAccountID: req.FromAccountID,
-		ToAccountID:   req.ToAccountID,
-		Amount:        req.Amount,
-	}
-
-	result, err := server.store.TransferTx(ctx, arg)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return
-	}
-
-	ctx.JSON(http.StatusOK, result)
-}
-
-func (server *Server) validAccount(ctx *gin.Context, accountID int64, currency string) (db.Account, bool) {
-	account, err := server.store.GetAccount(ctx, accountID)
-
-	if err != nil {
-		if err == sql.ErrNoRows {
-			ctx.JSON(http.StatusNotFound, errorResponse(err))
-			return account, false
-		}
-
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return account, false
-	}
-
-	if account.Currency != currency {
-		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", account.ID, account.Currency, currency)
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return account, false
-	}
-
-	return account, true
 }
---
+++ api/transfer.go
@@ -0,0 +1,79 @@
+package api
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/joefazee/simplebank/db/sqlc"
+	"github.com/joefazee/simplebank/token"
+)
+
+
+func (server *Server) createTransfer(ctx *gin.Context) {
+
+	var req transferRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	if req.FromAccountID == req.ToAccountID {
+		ctx.JSON(http.StatusBadRequest, errorResponse(errors.New("origin and destination account is the same")))
+		return
+	}
+
+	fromAccount, valid := server.validAccount(ctx, req.FromAccountID, req.Currency)
+	if !valid {
+		return
+	}
+
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if fromAccount.Owner != authPayload.Username {
+		ctx.JSON(http.StatusUnauthorized, errorResponse(errors.New("from account doesn`t belong to the authemticated user")))
+		return
+	}
+
+	if _, valid = server.validAccount(ctx, req.ToAccountID, req.Currency); !valid {
+		return
+	}
+	
+
+	arg := db.TransferTxParams{
+		FromAccountID: req.FromAccountID,
+		ToAccountID:   req.ToAccountID,
+		Amount:        req.Amount,
+	}
+
+	result, err := server.store.TransferTx(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, result)
+}
+
+func (server *Server) validAccount(ctx *gin.Context, accountID int64, currency string) (db.Account, bool) {
+	account, err := server.store.GetAccount(ctx, accountID)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return account, false
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return account, false
+	}
+
+	if account.Currency != currency {
+		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", account.ID, account.Currency, currency)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return account, false
+	}
+
+	return account, true
+}
